6a66a9c38e89a42c04b2dbc84bdd4c8262337fd8
Owajeul Islam <owajeul01357@gmail.com>
2024-08-05 06:37:32 +0600

    add Items controller

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,26 @@
+﻿using CollectionManager.Data_Access;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollectionManager.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly CollectionMangerDbContext _context;
+
+        public ItemsController(CollectionMangerDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int collectionId)
+        {
+            var collection = await _context.collections
+                .Include(c => c.Items)
+                .FirstOrDefaultAsync(x => x.Id == collectionId);
+
+            return Ok(collection);
+        }
+
+        
+    }
+}
