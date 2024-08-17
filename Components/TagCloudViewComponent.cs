cd45bb23bac2d8e7ec14772aaffa180866ab3225
Owajeul Islam <owajeul01357@gmail.com>
2024-08-17 11:21:55 +0600

    add TagCloud view component

---
+++ Components/TagCloudViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using CollectionManager.Data_Access.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectionManager.Components
+{
+    public class TagCloudViewComponent: ViewComponent
+    {
+        private readonly ItemRepository _itemRepository;
+
+        public TagCloudViewComponent(ItemRepository itemRepository)
+        {
+            _itemRepository = itemRepository;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var tags = await _itemRepository.GetTagCloudAsync();
+            return View(tags);
+        }
+    }
+}
